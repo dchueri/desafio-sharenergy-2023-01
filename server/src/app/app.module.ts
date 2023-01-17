d8af6147f4e64a4246ed25394ecea03e0f71dbce
diego <diegochueri@gmail.com>
2023-01-16 23:54:13 -0400

    feat(server): add mongoose

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ClientsModule } from 'src/clients/clients.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ClientsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    ClientsModule,
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
